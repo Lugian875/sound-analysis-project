6964baee16caa0b3db8b5699e2d889f8d12fcea0
Maxwell Benefield <maxminecraft109@gmail.com>
2024-11-26 15:25:43 -0500

    Button added now

--- gui.py
+++ gui.py
@@ -20,11 +20,11 @@ root.columnconfigure(7,weight=1)
 # Widgets
 title = tk.Label(
     root, text = 'Python Interactive Data Modeling Project', font = ('Arial 16 bold'), bg= 'white', fg= 'black'
-).grid(row=0,column=1,sticky='N')
+).grid(column=1,row=0,sticky='N')
 
-# audio_loader = tk.Button(
-#     frame, text = "Help me please :(", font=('Arial 12'), bg='white',fg='black'
-# ).grid(column=0,row=1)
+load_audio_btn = tk.Button(
+    root, text = "Load Audio", font=('Arial 12'), bg='white',fg='black'
+).grid(column=1,row=0,pady='40',sticky='N')
 
 
 root.mainloop() # Do not remove this line, as it is necessary for the window to pop up
