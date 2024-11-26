4a61accd94d3114ab8c3d4a8def672c007fa0bbb
Maxwell Benefield <maxminecraft109@gmail.com>
2024-11-26 15:29:02 -0500

    Added comments

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ root.title('SPIDAM Project')
 root.geometry('1366x768+300+300')
 root.resizable(True,True)
 
-#idk what this does
+#Configures Window Frame and Grid for Widget Organization
 frame = tk.Frame(root)
 root.rowconfigure(0,weight=1)
 root.columnconfigure(0,weight=1)
@@ -18,10 +18,12 @@ root.rowconfigure(7,weight=1)
 root.columnconfigure(7,weight=1)
 
 # Widgets
+# Title Label (centered at top)
 title = tk.Label(
     root, text = 'Python Interactive Data Modeling Project', font = ('Arial 16 bold'), bg= 'white', fg= 'black'
 ).grid(column=1,row=0,sticky='N')
 
+# Load Audio Button (40 pixels below Title Label)
 load_audio_btn = tk.Button(
     root, text = "Load Audio", font=('Arial 12'), bg='white',fg='black'
 ).grid(column=1,row=0,pady='40',sticky='N')
