2f475018514fed1a8e46f53d4bf376a075851900
Maxwell Benefield <maxminecraft109@gmail.com>
2024-11-27 13:23:43 -0500

    Load Audio Button now exectues a function and removes the button (for now)

--- gui.py
+++ gui.py
@@ -17,16 +17,25 @@ grid.grid(column=0,row=7,sticky='NESW',columnspan=2)
 root.rowconfigure(7,weight=1)
 root.columnconfigure(7,weight=1)
 
+# Place code for the audio loader here. Or it just goes to a differnt .py file?
+# That one is probably better for cleaner code and all that.
+def load_audio():
+    print("Audio Loader triggered. Now it just has to be coded.")
+    test_button = tk.Button(
+        root, text = "Test Button!", font=('Arial 32'),bg='darkolivegreen',fg='white'
+    ).grid(column=1,row=2)
+
 # Widgets
 # Title Label (centered at top)
 title = tk.Label(
     root, text = 'Python Interactive Data Modeling Project', font = ('Arial 16 bold'), bg= 'white', fg= 'black'
 ).grid(column=1,row=0,sticky='N')
 
-# Load Audio Button (40 pixels below Title Label)
+# Load Audio Button
 load_audio_btn = tk.Button(
-    root, text = "Load Audio", font=('Arial 12'), bg='white',fg='black'
-).grid(column=1,row=0,pady='40',sticky='N')
+    root, text = "Load Audio", font=('Arial 12'), bg='white',fg='black',command=lambda:[load_audio_btn.grid_forget(),load_audio()]
+)
+load_audio_btn.grid(column=1,row=0,pady='40',sticky='N')
 
 
 root.mainloop() # Do not remove this line, as it is necessary for the window to pop up
