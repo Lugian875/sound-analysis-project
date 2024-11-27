f1aea98a2ddae5039726c1247bd9ec3e2a8c3e79
Maxwell Benefield <maxminecraft109@gmail.com>
2024-11-27 14:10:55 -0500

    Moved code. Adjusted UI colors.

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+import audio_handler as ah
 
 root = tk.Tk()
 
 #Title, Default Window Size, and it is r
 root.title('SPIDAM Project')
-root.geometry('1366x768+300+300')
+root.geometry('1280x720+300+300')
 root.resizable(True,True)
 
 #Configures Window Frame and Grid for Widget Organization
@@ -19,23 +20,22 @@ root.columnconfigure(7,weight=1)
 
 # Place code for the audio loader here. Or it just goes to a differnt .py file?
 # That latter is probably better for cleaner code and all that.
-def load_audio():
-    print("Audio Loader triggered. Now it just has to be coded.")
-    test_button = tk.Button(
-        root, text = "Test Button!", font=('Arial 32'),bg='darkolivegreen',fg='white'
-    ).grid(column=1,row=2)
 
 # Widgets
 # Title Label (centered at top)
 title = tk.Label(
-    root, text = 'Python Interactive Data Modeling Project', font = ('Arial 16 bold'), bg= 'white', fg= 'black'
+    root, text = 'Python Interactive Data Modeling Project', font = ('Arial 32 bold'), fg= 'black'
 ).grid(column=1,row=0,sticky='N')
 
 # Load Audio Button
 load_audio_btn = tk.Button(
-    root, text = "Load Audio", font=('Arial 12'), bg='white',fg='black',command=lambda:[load_audio_btn.grid_forget(),load_audio()]
+    root, text = "Load Audio", font=('Arial 12'), fg='black',command=lambda:[load_audio_btn.grid_forget(),ah.load_audio(root)]
 )
-load_audio_btn.grid(column=1,row=0,pady='40',sticky='N')
+load_audio_btn.grid(column=1,row=0,pady='80',sticky='N')
+# Cleaner button?
 
+# Summary stats button
+
+# Plot switcher button
 
 root.mainloop() # Do not remove this line, as it is necessary for the window to pop up
