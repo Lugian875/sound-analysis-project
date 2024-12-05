4afc0eafd0237be8b3dadc69cd1e47a069fdb76c
apollo1270 <isaac.nashagh@gmail.com>
2024-12-05 17:48:03 -0500

    Added rRT60 frequencies to reporting
    added callback path
    added target frequency
    added difference for low/mid/high

--- reporting.py
+++ reporting.py
@@ -5,7 +5,8 @@ def generate_report(audio_path, callback):
     duration = audio_path["duration"]
     amplitude = audio_path["amplitude"]
     rt60 = audio_path["rt60"]
-
+    rt60_diff = audio_path["rt60_differences"]
+    
     callback(
         f"Audio Duration: {duration:.2f} seconds\n"
         f"Peak Amplitude: {amplitude:.2f}\n"
@@ -13,4 +14,8 @@ def generate_report(audio_path, callback):
         f"Low Frequency: {rt60['low']} seconds\n"
         f"Mid Frequency: {rt60['mid']} seconds\n"
         f"High Frequency: {rt60['high']} seconds\n"
+        f"RT60 Differences (Target = 0.5s):\n"
+        f"Low Frequency: {rt60_diff['low']}\n"
+        f"Mid Frequency: {rt60_diff['mid']}\n"
+        f"High Frequency: {rt60_diff['high']}\n"
     )
