689b719260308486a7c1cbd43803e0cbe0c67da2
Maxwell Benefield <maxminecraft109@gmail.com>
2024-12-04 20:25:53 -0500

    Made the code function

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 import audio_handler as ah
 
 root = tk.Tk()
@@ -18,6 +19,12 @@ grid.grid(column=0,row=7,sticky='NESW',columnspan=2)
 root.rowconfigure(7,weight=1)
 root.columnconfigure(7,weight=1)
 
+def load_audio():
+    load_audio_btn["state"] = 'disabled'
+    ah.load_audio()
+    load_audio_btn["state"] = 'normal'
+
+
 # Widgets
 # Title Label (centered at top)
 title = tk.Label(
@@ -26,12 +33,25 @@ title.grid(column=1, row=0, sticky='N')
 
 # Load Audio Button
 load_audio_btn = tk.Button(
-    root, text = "Load Audio", font='Arial 12', fg='black',command=lambda:[ah.load_audio()]
+    root, text="Load Audio", font='Arial 12', fg='black',command=lambda:[load_audio()]
 )
-load_audio_btn.grid(column=1,row=0,pady='80',sticky='N')
-# Cleaner button?
+load_audio_btn.grid(column=1,row=1,sticky='N')
 
-# Summary stats button
+# Audio Analysis Button
+audio_analysis_btn = tk.Button(
+    root, text="Analyze Audio", font='Arial 12',fg='black', state="disabled"
+)
+audio_analysis_btn.grid(column=1,row=2,pady=60, sticky='N')
+
+#Text Box
+status_box = tk.Text(
+    root, fg='black',font='Arial 10',wrap='word',height=5,width=32
+)
+status_box.grid(column=1,row=3,sticky='N')
+Test_String = "Test String"
+status_box.insert(tk.END,Test_String)
+status_box.config(state='disabled')
+
 
 # Plot switcher button
 
